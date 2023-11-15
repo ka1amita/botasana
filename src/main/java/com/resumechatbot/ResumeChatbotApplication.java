a7ba0e5e8c43011270ce8795c199b429472982e9
Matěj Kala <kalamatej@gmail.com>
2023-11-15 22:40:16 +0100

    Set-up GoogleChecks checkstyle and .env

--- src/main/java/com/resumechatbot/ResumeChatbotApplication.java
+++ src/main/java/com/resumechatbot/ResumeChatbotApplication.java
@@ -1,13 +1,17 @@
 package com.resumechatbot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResumeChatbotApplication {
+public class ResumeChatbotApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(ResumeChatbotApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+  }
 }
