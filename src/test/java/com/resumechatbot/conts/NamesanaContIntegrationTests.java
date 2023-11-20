f70e735c3dcd9574f37a79491779b85173c15bf9
Matěj Kala <kalamatej@gmail.com>
2023-11-20 23:53:44 +0100

    Create profile for NamesanaContIntegrationTests.java
    Create application-integration-tests.properties
    and use @ActiveProfiles("integration-tests") annotation

--- src/test/java/com/resumechatbot/conts/NamesanaContIntegrationTests.java
+++ src/test/java/com/resumechatbot/conts/NamesanaContIntegrationTests.java
@@ -11,21 +11,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("integration-tests")
 class NamesanaContIntegrationTests {
-  // note the usage of SECRET_OPENAI_API_TESTKEY! - see src/test/resources/application.properties
+  // note the usage of SECRET_OPENAI_API_TEST_KEY and OPENAI_API_CHAT_TEST_TEMPERATURE
+  //  - see src/test/resources/application-integration-tests.properties
   @Autowired
   private MockMvc mockMvc;
 
   @Test
   void returns_() throws Exception {
-    this.mockMvc.perform(get("/namesana").param("prompt", "Write the word \"test\""))
+    this.mockMvc.perform(get("/namesana").param("prompt", "Say Test"))
         .andDo(print())
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.completion").value("test"));
+        .andExpect(jsonPath("$.completion").value("Test"));
   }
 }
