5255ff328c3c393c482ebffedc7677baf38874d7
Matěj Kala <kalamatej@gmail.com>
2024-01-06 19:44:30 +0100

    Remove CommandLineRunner from BotasanaApplication class

--- src/main/java/com/botasana/BotasanaApplication.java
+++ src/main/java/com/botasana/BotasanaApplication.java
@@ -1,18 +1,14 @@
 package com.botasana;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan // because of ConfigurationProperties
 @SpringBootApplication
-public class BotasanaApplication implements CommandLineRunner {
+public class BotasanaApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(BotasanaApplication.class, args);
-  }
-
-  @Override
-  public void run(String... args) throws Exception {
   }
 }
